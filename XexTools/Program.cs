2795be9ed88f8cc06682e170d24fa51c4cea8c0f
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-06-12 11:05:09 -0700

    find save/restore reg funcs

--- XexTools/Program.cs
+++ XexTools/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection.PortableExecutable;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program {
     static void Main(string[] args) {
@@ -20,89 +23,11 @@ class Program {
         xex.Read(br);
 
         // 2. take the exe, and reformat it around the "virtual address" hacks
-        PEReader pr = new PEReader(new MemoryStream(xex.xexHeader.peImage));
+        XexPE xexPE = new XexPE();
+        xexPE.ImportExeFromXex(xex.xexHeader.peImage);
 
-        var headers = pr.PEHeaders;
-
-        List<byte> peAdjusted = new();
-
-        // duplicate the bytes up til the first section's PointerToRawData
-        for (int i = 0; i < headers.SectionHeaders[0].VirtualAddress; i++) {
-            peAdjusted.Add(xex.xexHeader.peImage[i]);
-        }
-        //Console.WriteLine($"Bytes 0 through 0x{headers.SectionHeaders[0].VirtualAddress:X} have been duplicated!");
-
-        for(int i = 0; i < headers.SectionHeaders.Length; i++) {
-            var section = headers.SectionHeaders[i];
-            //Console.WriteLine($"For section {section.Name}, I need to grab jeff.exe's data at 0x{section.VirtualAddress:X8}," +
-            //    $"and write the next 0x{section.SizeOfRawData:X8} bytes from it to the adjusted PE.\n" +
-            //    $"We're expecting the current index to write to to be 0x{section.PointerToRawData:X8}");
-            Debug.Assert(peAdjusted.Count == section.PointerToRawData, "Unexpected PE size at this point!");
-            List<byte> sectionBytes = new();
-            for(int j = 0; j < section.SizeOfRawData; j++) {
-                if(j + section.VirtualAddress >= xex.xexHeader.peImage.Length) {
-                    sectionBytes.Add(0);
-                }
-                else sectionBytes.Add(xex.xexHeader.peImage[j + section.VirtualAddress]);
-            }
-            peAdjusted.AddRange(sectionBytes);
-        }
-
-        // 3. analyze the reloc table
-        PEReader prGroundTruth = new PEReader(new MemoryStream(File.ReadAllBytes(groundTruth)));
-        var groundTruthRelocs = prGroundTruth.GetSectionData(".reloc");
-        var relocBytes = groundTruthRelocs.GetContent().ToArray();
-        Console.WriteLine($".reloc section size: 0x{relocBytes.Length:X} bytes");
-        BinaryReader relocReader = new BinaryReader(new MemoryStream(relocBytes));
-
-
-        while(relocReader.BaseStream.Position < relocReader.BaseStream.Length) {
-            var curPos = relocReader.BaseStream.Position;
-            uint pageRVA = relocReader.ReadUInt32();
-            uint blockSize = relocReader.ReadUInt32();
-
-            if (blockSize < 8 || relocReader.BaseStream.Position + blockSize > relocReader.BaseStream.Length) {
-                break; // invalid block, stop parsing
-            }
-
-            int entryCount = (int)((blockSize - 8) / 2);
-            Console.WriteLine($"Page RVA: 0x{pageRVA:X8}, Block Size: {blockSize}, Entry count: {entryCount}");
-
-            for(int i = 0; i < entryCount; i++) {
-                ushort entry = relocReader.ReadUInt16();
-                int type = entry >> 12;
-                int relocOffset = entry & 0xFFF;
-
-                if(pageRVA == 0x1000 || pageRVA == 0x2000)
-                    Console.WriteLine($"  Entry[{i}]: Type={type}, Offset=0x{relocOffset:X4}");
-            }
-            relocReader.BaseStream.Seek(curPos + blockSize, SeekOrigin.Begin);
-
-        }
-
-        //int offset = 0;
-        //while (offset < relocBytes.Length) {
-
-        //    uint pageRVA = BitConverter.ToUInt32(relocBytes, offset);
-        //    uint blockSize = BitConverter.ToUInt32(relocBytes, offset + 4);
-
-        //    if (blockSize < 8 || offset + blockSize > relocBytes.Length)
-        //        break; // invalid block, stop parsing
-
-        //    int entryCount = (int)((blockSize - 8) / 2);
-        //    Console.WriteLine($"Page RVA: 0x{pageRVA:X8}, Block Size: {blockSize}, Entry count: {entryCount}");
-
-        //    for (int i = 0; i < entryCount; i++) {
-        //        ushort entry = BitConverter.ToUInt16(relocBytes, offset + 8 + i * 2);
-        //        int type = entry >> 12;
-        //        int relocOffset = entry & 0xFFF;
-
-        //        Console.WriteLine($"  Entry[{i}]: Type={type}, Offset=0x{relocOffset:X4}");
-        //    }
-
-        //    offset += (int)blockSize;
-        //}
-
+        // 3. Control Flow Analysis
+        xexPE.FindFunctionBoundaries();
 
         //// Print Machine type
         //Console.WriteLine($"Machine enum: 0x{(ushort)headers.CoffHeader.Machine:X}");
@@ -135,6 +60,6 @@ class Program {
         //    Console.WriteLine();
         //}
 
-        //File.WriteAllBytes("D:\\DC3 Debug\\Gamepad\\Debug\\jeff_adjusted.exe", peAdjusted.ToArray());
+        //File.WriteAllBytes("D:\\DC3 Debug\\Gamepad\\Debug\\jeff_adjusted.exe", xexPE.exeBytes);
     }
 }
--- XexTools/Xex/XexPE.cs
+++ XexTools/Xex/XexPE.cs
@@ -4,7 +4,165 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection.PortableExecutable;
+using System.Diagnostics;
 
 class XexPE {
+    public class Function {
+        public string name;
+        public bool analyzed;
+        public uint rawByteArrayStart;
+        public uint rawByteArrayEnd;
+        // TODO: how do you calculate these?
+        public uint addressStart;
+        public uint addressEnd;
 
+        public Function() { }
+
+        public Function(string name, bool analyzed, uint rawByteArrayStart, uint rawByteArrayEnd, uint addressStart, uint addressEnd) {
+            this.name = name;
+            this.analyzed = analyzed;
+            this.rawByteArrayStart = rawByteArrayStart;
+            this.rawByteArrayEnd = rawByteArrayEnd;
+            this.addressStart = addressStart;
+            this.addressEnd = addressEnd;
+        }
+    }
+
+    public void ImportExeFromXex(byte[] xexPEImage) {
+        PEReader pr = new PEReader(new MemoryStream(xexPEImage));
+        var headers = pr.PEHeaders;
+
+        var optionalHeader = headers.PEHeader;
+        if(optionalHeader != null) {
+            Console.WriteLine($"AddressOfEntryPoint: 0x{optionalHeader.AddressOfEntryPoint:X8}");
+            Console.WriteLine($"ImageBase: 0x{optionalHeader.ImageBase:X8}");
+            entryPointAddress = (uint)optionalHeader.AddressOfEntryPoint;
+            imageBase = (uint)optionalHeader.ImageBase;
+        }
+        else {
+            throw new Exception("No optional header found. Is that even possible for an xex?");
+        }
+
+        List<byte> peAdjusted = new();
+
+        for(int i = 0; i < headers.SectionHeaders[0].VirtualAddress; i++) {
+            peAdjusted.Add(xexPEImage[i]);
+        }
+
+        for(int i = 0; i < headers.SectionHeaders.Length; i++) {
+            var section = headers.SectionHeaders[i];
+            if(section.Name == ".text") {
+                textSectionStart = (uint)section.PointerToRawData;
+                textSectionEnd = textSectionStart + (uint)section.SizeOfRawData;
+            }
+            Debug.Assert(peAdjusted.Count == section.PointerToRawData, "Unexpected PE size at this point!");
+            List<byte> sectionBytes = new();
+            for (int j = 0; j < section.SizeOfRawData; j++) {
+                if (j + section.VirtualAddress >= xexPEImage.Length) {
+                    sectionBytes.Add(0);
+                }
+                else sectionBytes.Add(xexPEImage[j + section.VirtualAddress]);
+            }
+            peAdjusted.AddRange(sectionBytes);
+        }
+
+        exeBytes = peAdjusted.ToArray();
+    }
+
+    private void FindSaveAndRestoreRegisterFuncs() {
+        // the asm corresponding to savegprlrs 14-17 and savefprs 14-17
+        // stands to reason that if we find these in sequence, 18-31 will be right behind
+        byte[] saveGPRasm = { 0xf9, 0xc1, 0xff, 0x68, 0xf9, 0xe1, 0xff, 0x70, 0xfa, 0x01, 0xff, 0x78, 0xfa, 0x21, 0xff, 0x80 };
+        byte[] saveFPRasm = { 0xd9, 0xcc, 0xff, 0x70, 0xd9, 0xec, 0xff, 0x78, 0xda, 0x0c, 0xff, 0x80, 0xda, 0x2c, 0xff, 0x88 };
+
+        int theSaveGPRIdx = -1;
+
+        for(int i = (int)textSectionStart; i < textSectionEnd - saveGPRasm.Length; i++) {
+            if (exeBytes.Skip(i).Take(saveGPRasm.Length).SequenceEqual(saveGPRasm)) {
+                //Console.WriteLine($"Found the savegpr funcs! They're at i = 0x{i:X}");
+                // evil hack: if we find the saveGPR sequence multiple times, take the last instance
+                theSaveGPRIdx = i;
+            }
+        }
+
+        if(theSaveGPRIdx == -1) {
+            throw new Exception("Save gpr compiler intrinsics not found. Is that even possible for an xex?");
+        }
+
+        for(int i = 14; i <= 31; i++, theSaveGPRIdx += 4) {
+            Function saveFunc = new Function($"__savegprlr_{i}", true, (uint)theSaveGPRIdx, (uint)theSaveGPRIdx + 4, 0, 0);
+            Function restoreFunc = new Function($"__restgprlr_{i}", true, (uint)theSaveGPRIdx + 0x48, (uint)theSaveGPRIdx + 0x4C, 0, 0);
+            functionBoundaries.Add(saveFunc);
+            functionBoundaries.Add(restoreFunc);
+        }
+
+        int theSaveFPRIdx = -1;
+
+        for (int i = (int)textSectionStart; i < textSectionEnd - saveFPRasm.Length; i++) {
+            if (exeBytes.Skip(i).Take(saveGPRasm.Length).SequenceEqual(saveFPRasm)) {
+                //Console.WriteLine($"Found the savefpr funcs! They're at i = 0x{i:X}");
+                // evil hack: if we find the saveFPR sequence multiple times, take the last instance
+                theSaveFPRIdx = i;
+            }
+        }
+
+        if (theSaveFPRIdx == -1) {
+            throw new Exception("Save fpr compiler intrinsics not found. Is that even possible for an xex?");
+        }
+
+        for (int i = 14; i <= 31; i++, theSaveFPRIdx += 4) {
+            Function saveFunc = new Function($"__savefpr_{i}", true, (uint)theSaveFPRIdx, (uint)theSaveFPRIdx + 4, 0, 0);
+            Function restoreFunc = new Function($"__restfpr_{i}", true, (uint)theSaveFPRIdx + 0x48, (uint)theSaveFPRIdx + 0x4C, 0, 0);
+            functionBoundaries.Add(saveFunc);
+            functionBoundaries.Add(restoreFunc);
+        }
+    }
+
+    public void FindFunctionBoundaries() {
+        // initial sweep to find and label the save/restore reg funcs
+        FindSaveAndRestoreRegisterFuncs();
+        BEBinaryReader br = new BEBinaryReader(new MemoryStream(exeBytes));
+        br.BaseStream.Seek(textSectionStart, SeekOrigin.Begin);
+
+        functionBoundaries.Sort((a, b) => a.rawByteArrayStart.CompareTo(b.rawByteArrayStart));
+        foreach (var func in functionBoundaries) {
+            Console.WriteLine($"Found {func.name} at byte offset 0x{func.rawByteArrayStart:X}");
+        }
+
+        // find function prologues to get known start addresses
+
+        // blr: 4e 80 00 20
+
+        //      -name: b
+        //  pattern: 0x48000000
+
+        //- name: bc
+        //  pattern: 0x40000000
+
+        //- name: bcctr
+        //  pattern: 0x4c000420
+
+        //- name: bclr
+        //  pattern: 0x4c000020
+
+        // this is where you try to find function boundaries
+
+        // so when you just have a giant.text section with a bunch of instructions, the most important thing is to determine function boundaries
+        // so CFA in this case means following the instructions control flow(branches, etc)
+        // with mwcc we know any bl target is the start of another function
+        // so we follow the control flow of a function until every path ends in a return (or, sometimes, a tail call)
+        // tail calls make things hard because they're just a b like you'd get with inner-function jumps, but they're actually pointing to a different fn
+        // dunno if you'll have to deal with tail call optimization with msvc 
+
+        // recursively going through and noting down every bl target
+        //the other part is following inner - function branches / jumps to find the end of the function
+        //and also resolving jump tables
+    }
+
+    public byte[] exeBytes;
+    public uint entryPointAddress;
+    public uint imageBase;
+    public uint textSectionStart;
+    public uint textSectionEnd;
+    public List<Function> functionBoundaries = new();
 }
